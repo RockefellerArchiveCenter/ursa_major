024fd55d51525413ac1a6a80e1207953888bbffe
Michael Nielsen <mnielsen@oswego.edu>
2018-07-05 17:01:38 -0400

    updated database to include date and time columns. Time is off by a few hours but will fix in next commit.

--- bagdiscovery/views.py
+++ bagdiscovery/views.py
@@ -7,30 +7,33 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 import uuid
+import datetime
+import time
+
 
 
 def index(request):
     if request.method == 'POST':
-        # print(receiveBag(request))
-        # print(uuid.uuid4())
-
-        json_data = json.loads(request.body.decode(encoding='UTF-8'))
-        json_bag = json.dumps(json_data)
-
-        # print('INSERT INTO bag VALUES (' + " ' " + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' "
-        #                + ')')
-
-        db = MySQLdb.connect(user='username', db='bagrepo', passwd='password', host='localhost')
-        cursor = db.cursor()
-        cursor.execute('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' "
-                       + ')')
-        db.commit()
-        db.close()
-
+        receiveBag(request)
     return render(request, 'discovery/index.html')
 
 
 def receiveBag(request):
+    now = datetime.datetime.now()
     print("I have received a POST request!")
+    json_data = json.loads(request.body.decode(encoding='UTF-8'))
+    json_bag = json.dumps(json_data)
+    "Current year: %d" % now.year
+
+    db = MySQLdb.connect(user='username', db='bagrepo', passwd='password', host='localhost')
+    cursor = db.cursor()
+    print('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
+          + "'" + "Hi" + "' ," + "'" + time.strftime('%Y-%m-%d %H:%M:%S') + "'")
+
+    cursor.execute('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
+                   + "'" + time.strftime('%m-%d-%Y') + "' ," + "'" + time.strftime('%I:%M:%S') + "')")
+
+    db.commit()
+    db.close()
 
     return 'Data is in the database!'
