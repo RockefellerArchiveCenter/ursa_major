37f4e9529acc4bcf066332ee243cd33f2f823cfd
Michael Nielsen <mnielsen@oswego.edu>
2018-07-05 15:54:52 -0400

    Added unique identifier to bags when being placed in database

--- bagdiscovery/views.py
+++ bagdiscovery/views.py
@@ -6,22 +6,31 @@ import MySQLdb
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
+import uuid
 
 
 def index(request):
     if request.method == 'POST':
-        print(receiveBag(request))
+        # print(receiveBag(request))
+        # print(uuid.uuid4())
+
+        json_data = json.loads(request.body.decode(encoding='UTF-8'))
+        json_bag = json.dumps(json_data)
+
+        # print('INSERT INTO bag VALUES (' + " ' " + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' "
+        #                + ')')
+
+        db = MySQLdb.connect(user='username', db='bagrepo', passwd='password', host='localhost')
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' "
+                       + ')')
+        db.commit()
+        db.close()
+
     return render(request, 'discovery/index.html')
 
 
 def receiveBag(request):
     print("I have received a POST request!")
-    json_data = json.loads(request.body.decode(encoding='UTF-8'))
-    json_bag = json.dumps(json_data)
-    db = MySQLdb.connect(user='username', db='dbname', passwd='password', host='localhost')
-    cursor = db.cursor()
-    cursor.execute('INSERT INTO t1 VALUES (' + " ' " + json_bag + " ' " + ')')
-    db.commit()
-    db.close()
 
     return 'Data is in the database!'
--- discovery/settings.py
+++ discovery/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'discovery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbname',
+        'NAME': 'bagrepo',
         'USER': 'username',
         'PASSWORD': 'password',
         'HOST': 'localhost',
