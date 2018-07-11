69aca34e1247d24c8d36cc647ff0b40a30c6099f
Michael Nielsen <mnielsen@oswego.edu>
2018-07-11 10:38:29 -0400

    Added GET functionality on bagdiscovery page

--- bagdiscovery/views.py
+++ bagdiscovery/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,33 +8,42 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 import uuid
-import datetime
 import time
-
 
 
 def index(request):
     if request.method == 'POST':
         receiveBag(request)
+    elif request.GET.get('endpoint'):
+        getBag(request)
     return render(request, 'discovery/index.html')
 
 
 def receiveBag(request):
-    now = datetime.datetime.now()
     print("I have received a POST request!")
     json_data = json.loads(request.body.decode(encoding='UTF-8'))
     json_bag = json.dumps(json_data)
-    "Current year: %d" % now.year
-
-    db = MySQLdb.connect(user='username', db='bagrepo', passwd='password', host='localhost')
-    cursor = db.cursor()
-    print('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
-          + "'" + "Hi" + "' ," + "'" + time.strftime('%Y-%m-%d %H:%M:%S') + "'")
-
-    cursor.execute('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
-                   + "'" + time.strftime('%m-%d-%Y') + "' ," + "'" + time.strftime('%I:%M:%S') + "')")
-
-    db.commit()
-    db.close()
+    storeBag(json_bag)
 
     return 'Data is in the database!'
+
+
+def getBag(request):
+    print('YOOOOOOOOOOOOOOO')
+    print(request.GET.get('endpoint'))
+    response = requests.get(request.GET.get('endpoint'))
+    json_bag = str(response.json())
+    storeBag(json_bag)
+
+
+def storeBag(json_bag):
+        db = MySQLdb.connect(user='username', db='bagrepo', passwd='password', host='localhost')
+        cursor = db.cursor()
+        print('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
+              + "'" + "Hi" + "' ," + "'" + time.strftime('%Y-%m-%d %H:%M:%S') + "'")
+
+        cursor.execute('INSERT INTO bag VALUES (' + " '" + uuid.uuid4().__str__() + " ' , '[" + json_bag + "]' , "
+                       + "'" + time.strftime('%m-%d-%Y') + "' ," + "'" + time.strftime('%I:%M:%S %p') + "')")
+
+        db.commit()
+        db.close()
