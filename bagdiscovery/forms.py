231a5daefdf8af77377a5f3389e7896bcd6a1e7f
Michael Nielsen <mnielsen@oswego.edu>
2018-09-05 16:48:16 -0400

    Added functionality for endpoint to accept ZIP files in prep to receive them from Aurora. Does not save to database yet.

--- bagdiscovery/forms.py
+++ bagdiscovery/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class URLForm(forms.Form):
     endpoint = forms.CharField()
+
+
+class ZIPForm(forms.Form):
+    zip = forms.FileField()
--- bagdiscovery/views.py
+++ bagdiscovery/views.py
@@ -1,9 +1,12 @@
+import io
 import json
 import urllib.request
+import zipfile
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .forms import URLForm
+from .forms import URLForm, ZIPForm
 from .library import receiveBag, storeNewBag, getBags
+import gzip
 
 
 class index(TemplateView):
@@ -13,13 +16,32 @@ class index(TemplateView):
     def post(self, request):
         rows = getBags()
         form = URLForm(request.POST)
+
+        # This is the post from the form on the page
+
         if request.method == 'POST' and form.is_valid():
             myform = form.cleaned_data['endpoint']
             response = urllib.request.urlopen(myform)
             data = str(json.loads(response.read().decode(encoding='UTF-8')))
             storeNewBag(data)
+
+        # This is from a POST request
+
         else:
-            receiveBag(request)
+            r = request.body
+            print(type(r))
+            print(r)
+
+            # z = bytes([r])
+
+            zf = zipfile.ZipFile(io.BytesIO(r), "r")
+            print(zf.namelist())
+
+            for fileinfo in zf.infolist():
+                print(zf.namelist())
+                print(zf.read(fileinfo).decode('utf-8'))
+
+            # receiveBag(request)
 
         return render(request, template_name="bagdiscovery/index.html")
 
