8b40fbea56568d4a7e27c16f1f267aafee40732c
Hillel Arnold <helrond@hotmail.com>
2018-09-26 19:23:29 -0400

    update allowed hosts

--- ursa_major/settings.py
+++ ursa_major/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'de#zj*$s(y%&1xd)dv#=7=#vz_(&l_3g)%)y#sgjykx0!!is=s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'ursa-major-web']
+ALLOWED_HOSTS = ['localhost', 'ursa-major-web']
 
 
 # Application definition
