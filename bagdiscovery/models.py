11c33dbfd4b26d1824f07979f054157d7894c1e3
Hillel Arnold <helrond@hotmail.com>
2020-01-27 13:20:27 -0500

    make accession non-required, fixes #87

--- bagdiscovery/models.py
+++ bagdiscovery/models.py
@@ -11,7 +11,7 @@ class Accession(models.Model):
 class Bag(models.Model):
     bag_identifier = models.CharField(max_length=255, unique=True)
     bag_path = models.CharField(max_length=255, null=True, blank=True)
-    accession = models.ForeignKey(Accession, on_delete=models.CASCADE)
+    accession = models.ForeignKey(Accession, on_delete=models.CASCADE, null=True, blank=True)
     data = JSONField(null=True, blank=True)
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
